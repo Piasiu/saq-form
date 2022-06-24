716e886a054a1f30a2e00dda987a89b61a76b891
Piasiu <pawel@piaskowy.net>
2022-06-24 19:47:59 +0200

    fix

--- src/CsrfField.php
+++ src/CsrfField.php
@@ -25,14 +25,29 @@ class CsrfField extends FormElement implements FieldInterface
      */
     public function __construct(string $salt)
     {
+        $this->setSalt($salt);
+        $this->formName = $this->getName();
+    }
+
+    /**
+     * @param string $salt
+     */
+    public function setSalt(string $salt): void
+    {
         $this->salt = $salt;
 
         if (!array_key_exists('csrf', $_SESSION) || !array_key_exists($salt, $_SESSION['csrf']))
         {
             $_SESSION['csrf'][$salt] = '';
         }
+    }
 
-        $this->formName = $this->getName();
+    /**
+     * @return string
+     */
+    public function getSalt(): string
+    {
+        return $this->salt;
     }
 
     /**
